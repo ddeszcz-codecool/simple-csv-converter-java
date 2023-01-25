d78de7e44e1545bb1b12120711ffa128e19663cf
Dawid Deszcz <d.deszcz@gmail.com>
2023-01-25 12:53:44 +0100

    Step 3: No input file action |||

--- src/main/java/com/codecool/scc/ConverterApplication.java
+++ src/main/java/com/codecool/scc/ConverterApplication.java
@@ -7,7 +7,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConverterApplication {
 
     public static void main(String[] args) {
+
+        System.out.println("\n\n------------------------------- CONFIGURATION STARTS -------------------------------\n\n");
+
         SpringApplication.run(ConverterApplication.class, args);
+
+        System.out.println("\n\n---------------------------CONFIGURATION ENDS, APP STARTs --------------------------\n\n");
+
+
+        switch (args.length) {
+            case 0:
+                System.out.println("No input file defined");
+                break;
+            default:
+                System.out.println("Invalid number of arguments. Enter only one or two arguments");
+        }
+
+
     }
 
 }
+
+
